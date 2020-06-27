d41dd4cb455a06011b9b80577c78db58a8007a29
Fabien Crespel <fabien@crespel.net>
2020-06-27 18:47:37 +0200

    Infra: update cleanup.sh

--- infrastructure/commandcentral-builder/cleanup.sh
+++ infrastructure/commandcentral-builder/cleanup.sh
@@ -3,33 +3,25 @@
 echo "Cleaning up $SAG_HOME ..."
 cd $SAG_HOME
 
-echo 'Disk usage before cleaning:'
+echo "Disk usage before cleaning:"
 du -h -d 2
 
-echo "Removing Update Manager but keep metadata (SAGUpdateManager/UpdateManager/profile/p2) ..."
-cd SAGUpdateManager && rm -rf bin jvm logs osgi && \
-cd UpdateManager && rm -rf bootstrap conf logs readme repository workdata restart_script && \
-cd profile && rm -rf *.xml *.ini configuration plugins && \
-cd $SAG_HOME
+echo "Removing Update Manager ..."
+rm -fr SAGUpdateManager
 
 echo "Removing Installer but keep install/products metadata ..."
 rm -fr install/repo install/logs install/jars install/bpms install/etc install/fix/backup install/fix/profile/org.eclipse.equinox.p2.core/cache
 rm -f sagProducts.xml sagMetadata.xml p2.index
 
 echo "Removing Licenses and doc ..."
-#rm -fr Licenses/
-rm -fr _documentation/
+rm -fr Licenses/ _documentation/
 
 echo "Removing log files ..."
 rm -f `find ./ -name *.log`
-
-# echo "Removing Common ..."
-# rm -rf common/db/
-# rm -rf common/runtime/install/
-# rm -rf common/runtime/agent/
+rm -fr cc-boot/
 
 echo "Removing Java ..."
 rm -fr jvm/
 
-echo 'Disk usage after cleaning:'
+echo "Disk usage after cleaning:"
 du -h -d 2
