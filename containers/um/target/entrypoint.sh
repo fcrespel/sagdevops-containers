9206f1eb91d67f191e7621c53ca50e1ff6842fea
Fabien Crespel <fabien@crespel.net>
2020-07-03 17:34:39 +0200

    Update UM container with data volume and graceful shutdown

--- containers/um/target/entrypoint.sh
+++ containers/um/target/entrypoint.sh
@@ -5,5 +5,12 @@ if [ "$CC_AUTO_REGISTER" = "1" ]; then
     $SAG_HOME/register.sh
 fi
 
+# Configure shutdown handler
+trap "$UM_HOME/server/$UM_REALM/bin/nstopserver" SIGINT SIGTERM
+
 # Start server
-exec $UM_HOME/server/$UM_REALM/bin/nserverdaemon console
+exec $UM_HOME/server/$UM_REALM/bin/nserverdaemon console "$@" &
+
+# Wait for shutdown
+PID=$!
+wait $PID
