8511f0188f2a8e62328ee53b7b989b061d7fb691
Fabien Crespel <fabien@crespel.net>
2020-07-17 00:15:34 +0200

    Containers: use external Elasticsearch for apigw and support env vars

--- containers/apigw/target/entrypoint.sh
+++ containers/apigw/target/entrypoint.sh
@@ -5,6 +5,9 @@ if [ "$CC_AUTO_REGISTER" = "1" ]; then
     $SAG_HOME/register.sh
 fi
 
+# Process environment variables
+java -cp $SAG_HOME/IntegrationServer/instances/apigateway/packages/WmAPIGateway/bin/lib/apigateway-tools.jar com.softwareag.apigateway.tools.docker.ModifyExternalProperties apigateway
+
 # Configure shutdown handler
 trap "$SAG_HOME/profiles/IS_apigateway/bin/shutdown.sh" SIGINT SIGTERM
 
