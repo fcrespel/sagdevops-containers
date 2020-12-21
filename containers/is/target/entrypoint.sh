dd32cf7bbdb3b470d0c6a398969cbf991c597749
Fabien Crespel <fabien@crespel.net>
2020-12-21 21:42:16 +0100

    IS: add memory config, fix local server alias hostname

--- containers/is/target/entrypoint.sh
+++ containers/is/target/entrypoint.sh
@@ -5,6 +5,10 @@ if [ "$CC_AUTO_REGISTER" = "1" ]; then
     $SAG_HOME/register.sh
 fi
 
+# Update config
+[ -n "$JAVA_MIN_MEM" ] && sed -i "s#^wrapper.java.initmemory=.*\$#wrapper.java.initmemory=$JAVA_MIN_MEM#g" $SAG_HOME/profiles/IS_default/configuration/custom_wrapper.conf
+[ -n "$JAVA_MAX_MEM" ] && sed -i "s#^wrapper.java.maxmemory=.*\$#wrapper.java.maxmemory=$JAVA_MAX_MEM#g" $SAG_HOME/profiles/IS_default/configuration/custom_wrapper.conf
+
 # Configure shutdown handler
 trap "$SAG_HOME/profiles/IS_default/bin/shutdown.sh" SIGINT SIGTERM
 
