78a878aef839df3a6363c9068d914e7e6ddda0e8
Fabien Crespel <fabien@crespel.net>
2021-01-18 16:06:48 +0100

    Containers: add memory config for API GW + UM

--- containers/apigw/target/entrypoint.sh
+++ containers/apigw/target/entrypoint.sh
@@ -5,6 +5,10 @@ if [ "$CC_AUTO_REGISTER" = "1" ]; then
     $SAG_HOME/register.sh
 fi
 
+# Update config
+[ -n "$JAVA_MIN_MEM" ] && sed -i "s#^wrapper.java.initmemory=.*\$#wrapper.java.initmemory=$JAVA_MIN_MEM#g" $SAG_HOME/profiles/IS_apigateway/configuration/custom_wrapper.conf
+[ -n "$JAVA_MAX_MEM" ] && sed -i "s#^wrapper.java.maxmemory=.*\$#wrapper.java.maxmemory=$JAVA_MAX_MEM#g" $SAG_HOME/profiles/IS_apigateway/configuration/custom_wrapper.conf
+
 # Process environment variables
 java -cp $SAG_HOME/IntegrationServer/instances/apigateway/packages/WmAPIGateway/bin/lib/apigateway-tools.jar com.softwareag.apigateway.tools.docker.ModifyExternalProperties apigateway
 
--- containers/um/target/entrypoint.sh
+++ containers/um/target/entrypoint.sh
@@ -5,11 +5,15 @@ if [ "$CC_AUTO_REGISTER" = "1" ]; then
     $SAG_HOME/register.sh
 fi
 
+# Update config
+[ -n "$JAVA_MIN_MEM" ] && sed -i "s#^wrapper.java.initmemory=.*\$#wrapper.java.initmemory=$JAVA_MIN_MEM#g" $UM_HOME/server/$UM_REALM/bin/Custom_Server_Common.conf
+[ -n "$JAVA_MAX_MEM" ] && sed -i "s#^wrapper.java.maxmemory=.*\$#wrapper.java.maxmemory=$JAVA_MAX_MEM#g" $UM_HOME/server/$UM_REALM/bin/Custom_Server_Common.conf
+
 # Configure shutdown handler
 trap "$UM_HOME/server/$UM_REALM/bin/nstopserver" SIGINT SIGTERM
 
 # Start server
-exec $UM_HOME/server/$UM_REALM/bin/nserverdaemon console "$@" &
+$UM_HOME/server/$UM_REALM/bin/nserverdaemon console "$@" &
 
 # Wait for shutdown
 PID=$!
