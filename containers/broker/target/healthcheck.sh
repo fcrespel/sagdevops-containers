ccc4f946980336ed82125ef084b9aa02da98e29c
Fabien Crespel <fabien@crespel.net>
2022-05-08 23:24:45 +0200

    Broker: ignore Pause Publishing in healthcheck

--- containers/broker/target/healthcheck.sh
+++ containers/broker/target/healthcheck.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
 $SAG_HOME/Broker/bin/broker_ping "${BROKER_NAME:-default}@localhost:6849" -timeout 10
+RET=$?
+if [ $RET -eq 115 ]; then
+    # Ignore Pause Publishing
+    RET=0
+fi
+exit $RET
